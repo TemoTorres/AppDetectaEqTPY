711870bc1074a41ecc80871a7d5dd64895b9cb2c
TemoTorres <temotorres3105@gmail.com>
2026-03-05 10:43:31 -0700

    Add files via upload

--- AppDetectaEqTPY_sw.js
+++ AppDetectaEqTPY_sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'appdetectaeqtpy-v1';
+const CACHE_NAME = 'appdetectaeqtpy-v3';
 const ASSETS = [
     './index.html',
     './AppDetectaEqTPY_dashboard.html',
@@ -12,6 +12,24 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(ASSETS);
+        }).then(() => {
+            return self.skipWaiting();
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        }).then(() => {
+            return self.clients.claim();
         })
     );
 });
